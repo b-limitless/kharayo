a473c91f5da7132997ea4199b62039cf63629727
bharat shah <akashche72@gmail.com>
2021-07-24 20:01:50 +0400

    Update mutation is created

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -28,4 +28,21 @@ export class PostResolver {
         await em.persistAndFlush(post);
         return post;
     }
+
+    @Mutation(() => Post, {nullable: true}) 
+    async updatePost(
+        @Arg('id') id: number,
+        @Arg('title' , () => String, {nullable: true}) title: string,
+        @Ctx() {em}: MyContext
+    ): Promise<Post | null> {
+        const post = await em.findOne(Post, {id});
+        if(!post) {
+            return null;
+        }
+        if(typeof title !== 'undefined') {
+            post.title = title;
+            await em.persistAndFlush(post);
+        }
+        return post;
+    }
 }
