ba57c804d1150cf3ea4dc09887697b058ab2f6d1
bharat shah <akashche72@gmail.com>
2021-07-24 18:59:18 +0400

    GrapQL ApploServer is created with express

--- src/entities/Post.ts
+++ src/entities/Post.ts
@@ -6,13 +6,13 @@ export class Post {
   @PrimaryKey()
   id!: number;
 
-  @Property()
+  @Property({type: 'date'})
   createdAt: Date = new Date();
 
-  @Property({ onUpdate: () => new Date() })
+  @Property({type: 'date', onUpdate: () => new Date() })
   updatedAt: Date = new Date();
 
-  @Property()
+  @Property({type: 'text'})
   title!: string;
 
 }
--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,29 @@ import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
 import { Post } from "./entities/Post";
 import mikroOrmConfig from "./mikro-orm.config";
-
+import express from 'express';
+import {ApolloServer} from 'apollo-server-express';
+import {buildSchema} from 'type-graphql';
+import { HelloResolver } from "./resolvers/hello";
 export const main = async() => {
     const orm = await MikroORM.init(mikroOrmConfig);
-    const post = orm.em.create(Post, {title: 'hello world'});
-    await orm.em.persistAndFlush(post);
-    return orm;
+    await orm.getMigrator().up();
+    
+    const app = express();
+
+    const apolloServer = new ApolloServer({
+        schema: await buildSchema({
+            resolvers: [HelloResolver],
+            validate: false
+        }),
+    });
+    await apolloServer.start();
+    apolloServer.applyMiddleware({app});
+
+    app.listen(4000, () => {
+        console.log('server started on localhost:')
+    });
+   
 }
 
 main().catch(err => {
