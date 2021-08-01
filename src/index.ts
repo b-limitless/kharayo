7d49f1e2185b7db8bf4f673a7099f3e7e65444e4
bharat shah <akashche72@gmail.com>
2021-08-01 18:08:35 +0400

    Login mutation is created in login resolver and in front endside mutation is added to graphql/muations and then yarn run gen ran to create generate logoutMutation

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,76 @@
 import "reflect-metadata";
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
-import { Post } from "./entities/Post";
 import mikroOrmConfig from "./mikro-orm.config";
-import express from 'express';
-import {ApolloServer} from 'apollo-server-express';
-import {buildSchema} from 'type-graphql';
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
 import { HelloResolver } from "./resolvers/hello";
 import { PostResolver } from "./resolvers/post";
 import { UserResolver } from "./resolvers/user";
-export const main = async() => {
-    const orm = await MikroORM.init(mikroOrmConfig);
-    await orm.getMigrator().up();
-    
-    const app = express();
+import redis from "redis";
+import session from "express-session";
+import connectRedis from "connect-redis";
+import cors from 'cors';
+import { MyContext } from "./types";
+import { COOKIE_NAME } from "./constants";
+export const main = async () => {
+  const orm = await MikroORM.init(mikroOrmConfig);
+  await orm.getMigrator().up();
 
-    const apolloServer = new ApolloServer({
-        schema: await buildSchema({
-            resolvers: [HelloResolver, PostResolver, UserResolver],
-            validate: false
+  const app = express();
+
+  const RedisStore = connectRedis(session);
+  const redisClient = redis.createClient();
+
+  app.use(cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  }));
+  
+  app.use(
+    session({
+      name: COOKIE_NAME,
+      store: new RedisStore({ 
+           client: redisClient,
+           disableTTL: true,
+           disableTouch: true
         }),
-        context: () => ({
-            em: orm.em
-        })
-    });
-    await apolloServer.start();
-    apolloServer.applyMiddleware({app});
+      cookie: {
+          maxAge: 1000 * 60 * 60 * 24 * 365 * 10, // 10 years 
+          httpOnly: true,
+          sameSite: 'lax', // csrf 
+          secure: __prod__ // cookie only work in https
+        },
+      saveUninitialized: false,
+      secret: "secret",
+      resave: false,
+    })
+  );
 
-    app.listen(4000, () => {
-        console.log('server started on localhost:')
-    });
-   
-}
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver, PostResolver, UserResolver],
+      validate: false,
+    }),
+    
+    context: ({req, res}): MyContext => ({
+      em: orm.em,
+      req,
+      res
+    }),
+  });
+  await apolloServer.start();
+  apolloServer.applyMiddleware({ 
+    app,
+    cors: false
+  });
 
-main().catch(err => {
-    console.log(err)
+  app.listen(4000, () => {
+    console.log("server started on localhost:");
+  });
+};
+
+main().catch((err) => {
+  console.log(err);
 });
--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -7,10 +7,11 @@ import {
   Arg,
   Ctx,
   ObjectType,
+  Query,
 } from "type-graphql";
 import { User } from "../entities/User";
 import argon2 from "argon2";
-import { fileURLToPath } from "url";
+import { COOKIE_NAME } from "../constants";
 
 @InputType()
 class UsernamePasswordInput {
@@ -40,10 +41,21 @@ class UserResponse {
 
 @Resolver()
 export class UserResolver {
+  @Query(() => User, {nullable: true})
+  async me(@Ctx() {req, em}: MyContext) {
+    if(!req.session.userId) {
+      return null;
+    }
+    const user = await em.findOne(User, {id:  req.session.userId})
+    
+    return user;
+
+  }
+
   @Mutation(() => UserResponse)
   async register(
     @Arg("options", () => UsernamePasswordInput) options: UsernamePasswordInput,
-    @Ctx() { em }: MyContext
+    @Ctx() { em, req }: MyContext
   ): Promise<UserResponse> {
     if (options.username.length <= 2) {
       return {
@@ -56,7 +68,7 @@ export class UserResolver {
       };
     }
 
-    if (options.username.length <= 3) {
+    if (options.password.length <= 3) {
       return {
         errors: [
           {
@@ -73,7 +85,15 @@ export class UserResolver {
       username: options.username,
       password: hashedPassword,
     });
+    //let user;
     try {
+      // const [result] = await (em as EntityManager).createQueryBuilder(User).getKnexQuery().insert({
+      //   username: options.username,
+      //   password: hashedPassword,
+      //   created_at: new Date(),
+      //   updated_at: new Date()
+      // }).returning("*");
+      // user = result[0];
       await em.persistAndFlush(user);
     } catch(err){
       if(err.code === "23505") {
@@ -88,14 +108,14 @@ export class UserResolver {
 
       }
     }
-    
+    req.session!.userId = user.id;
     return { user };
   }
 
   @Mutation(() => UserResponse)
   async login(
     @Arg("options", () => UsernamePasswordInput) options: UsernamePasswordInput,
-    @Ctx() { em }: MyContext
+    @Ctx() { em, req }: MyContext
   ): Promise<UserResponse> {
     const user = await em.findOne(User, { username: options.username });
     if (!user) {
@@ -119,9 +139,25 @@ export class UserResolver {
         ],
       };
     }
-    //await em.persistAndFlush(user);
+    req.session!.userId = user.id;
     return {
       user,
     };
+  }
+
+  @Mutation(() => Boolean)
+  logout(
+    @Ctx() {req, res}: MyContext
+  ) {
+      return new Promise(resolve => req.session.destroy(err => {
+        res.clearCookie(COOKIE_NAME);
+        if(err) {
+          console.log(err);
+          resolve(false);
+          return;
+        }
+
+        resolve(true);
+      }))
   }
 }
